ae1e5a9e408284b59caa0578ffa3a4e52dc53378
arshlaanhaq <arshlaanhaq005@.com>
2022-10-09 23:06:03 +0530

    Initial commit

---
+++ static/index.js
@@ -0,0 +1,17 @@
+alert(`hello buddy!!!
+this site is made for display purpose only!   
+Have Fun :)`);
+
+let navbar = document.querySelector("nav");
+let navigation = document.querySelector("#navbar li a");
+let menutgl = document.querySelector(".menutgl");
+
+menutgl.onclick = function(){
+    navbar.classList.toggle('open');
+}
+
+
+
+
+
+
